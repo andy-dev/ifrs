97b9bf8d6e8e8dd43d174e6a03c1259dde7eabcf
Andres Rodriguez Macedo <andressmacedosolana@Andress-MBP.attlocal.net>
2020-03-10 18:54:18 -0500

    added calendar picker, added user input, added update change on text areas

--- src/user/containers/form-container.js
+++ src/user/containers/form-container.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import { Router, Link, navigate } from "@reach/router";
 import { firestore } from "../../firebase";
+import Calendar from "./components/calendar.js";
 
 class FormContainer extends Component {
   state = {
-    form: null
+    form: null,
+    userResponses: null
   };
 
   unsubscribeFromForm = null;
@@ -24,12 +26,18 @@ class FormContainer extends Component {
     this.unsubscribeFromForm = this.userFormsRef
       .doc(docId)
       .onSnapshot(snapshot => {
-        console.log(snapshot);
         const form = { ...snapshot.data(), id: docId };
-        console.log("form", form);
-        this.setState({ form });
+
+        const userResponses = this.arrayToObject(form.form);
+        this.setState({ form, userResponses });
       });
   };
+
+  arrayToObject = array =>
+    array.reduce((obj, item) => {
+      obj[item.rank] = item.userResponse;
+      return obj;
+    }, {});
 
   componentDidUpdate() {
     const { form } = this.state;
@@ -44,8 +52,17 @@ class FormContainer extends Component {
     }
   };
 
+  handleChange = event => {
+    // name ~ rank
+    const { name, value } = event.target;
+    const { userResponses } = this.state;
+
+    userResponses[parseInt(name, 10)] = value;
+    this.setState({ userResponses });
+  };
+
   render() {
-    const { form } = this.state;
+    const { form, userResponses } = this.state;
     return (
       <>
         <h1>Form</h1>
@@ -54,13 +71,47 @@ class FormContainer extends Component {
 
         {form !== null &&
           form.form.map(q => {
-            return (
-              <ul key={q.rank}>
-                <li>{q.question}</li>
-                <li>{q.userResponse}</li>
-                <li>{q.rank}</li>
-              </ul>
-            );
+            if (q.inputType === "string") {
+              return (
+                <div key={q.rank}>
+                  <label htmlFor={q.rank}>{q.question}</label>
+                  <br />
+                  <textarea
+                    id={q.rank}
+                    name={q.rank}
+                    rows="5"
+                    cols="33"
+                    value={userResponses[q.rank]}
+                    onChange={this.handleChange}
+                  ></textarea>
+                </div>
+              );
+            } else if (q.inputType === "date") {
+              return (
+                <div key={q.rank}>
+                  <p>{q.question}</p>
+                  <br />
+                  <Calendar></Calendar>
+                </div>
+              );
+            } else if (q.inputType === "number") {
+              return (
+                <div key={q.rank}>
+                  <p>{q.question}</p>
+                  <input type="number" />
+                </div>
+              );
+            } else if (q.inputType === "boolean") {
+              return (
+                <div key={q.rank}>
+                  <p>{q.question}</p>
+                  <label htmlFor="huey">Si</label>
+                  <input type="radio" id="huey" name="drone" value="si" />
+                  <label htmlFor="huey">No</label>
+                  <input type="radio" id="huey" name="drone" value="no" />
+                </div>
+              );
+            }
           })}
         {/* <pre>
           <code>{JSON.stringify(form, null, 4)}</code>
